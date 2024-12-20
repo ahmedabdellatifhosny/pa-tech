38ca581a22e9a9e26595e239883bd5728f1fc5fd
ahmed abdellatif <ahmedabdellatifhosny@gmail.com>
2024-12-20 01:59:47 -0800

    update formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import {Route, createRoutesFromElements, RouterProvider, createBrowserRouter} from 'react-router-dom'
-import './App.css'
-import ClientLayout from './layouts/ClientLayout'
+import {
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+import "./App.css";
+import ClientLayout from "./layouts/ClientLayout";
 
 function App() {
-
   const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path='/' element={<ClientLayout />} />
-    )
-  )
+    createRoutesFromElements(<Route path="/" element={<ClientLayout />} />)
+  );
 
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
 
 export default App;
